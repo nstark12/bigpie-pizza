36fbd44fc954f7613d85059ac29952cacbce9c5f
Marketing Director <nicolestark1103@gmail.com>
2023-12-07 13:44:20 -0600

    added a coupe routes now working on register page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,19 +4,22 @@ export default function Header() {
   return (
     <>
       <header className="flex items-center justify-between">
-        <Link className="text-primary font-semibold text-2xl" href="/">
-          bigPie Pizza
-        </Link>
         <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-          <Link href={""}>Home</Link>
+          <Link className="text-primary font-semibold text-2xl" href={"/"}>
+            bigPie Pizza
+          </Link>
+          <Link href={"/"}>Home</Link>
           <Link href={""}>Menu</Link>
           <Link href={""}>About</Link>
           <Link href={""}>Contact</Link>
+        </nav>
+        <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+          <Link href={"/login"}>Login</Link>
           <Link
             className="bg-primary text-white px-8 py-2 rounded-full"
-            href={""}
+            href={"/register"}
           >
-            Login
+            Register
           </Link>
         </nav>
       </header>
