58125b33ccd258311f7ded056a90355a78e3fb9f
Marketing Director <nicolestark1103@gmail.com>
2023-12-08 13:23:03 -0600

    image change working but not saving on refresh

---
+++ src/app/api/profile/route.js
@@ -0,0 +1,26 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { User } from "@/models/User";
+import mongoose from "mongoose";
+import { getServerSession } from "next-auth";
+
+export async function PUT(req) {
+  mongoose.connect(process.env.MONGODB_URL);
+  const data = await req.json();
+  const session = await getServerSession(authOptions);
+  const email = session.user.email;
+
+  const update = {};
+  if ("name" in data) {
+    update.name = data.name;
+  }
+
+  if ("image" in data) {
+    update.image = data.image;
+  }
+
+  if (Object.keys(update).length > 0) {
+    await User.updateOne({ email }, { update });
+  }
+
+  return Response.json(true);
+}
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,7 +1,113 @@
+"use client";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function ProfilePage() {
+  const session = useSession();
+  const [userName, setUserName] = useState("");
+  const [image, setImage] = useState("");
+  const [saved, setSaved] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
+  const { status } = session;
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      setUserName(session.data.user.name);
+      setImage(session.data.user.image);
+    }
+  }, [session, status]);
+
+  async function handleProfileInfoUpdate(e) {
+    e.preventDefault();
+    setSaved(false);
+    setIsSaving(true);
+    const response = await fetch("/api/profile", {
+      method: "PUT",
+      body: JSON.stringify({ name: userName, image }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setIsSaving(false);
+    if (response.ok) {
+      setSaved(true);
+    }
+  }
+
+  async function handleFileChange(e) {
+    const files = e.target.files;
+    if (files?.length === 1) {
+      const data = new FormData();
+      data.set("file", files[0]);
+      const response = await fetch("/api/upload", {
+        method: "POST",
+        body: data,
+      });
+      const link = await response.json();
+      setImage(link);
+    }
+  }
+
+  if (status === "loading") {
+    return "Loading...";
+  }
+
+  if (status === "unauthenticated") {
+    return redirect("/login");
+  }
+
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl">Profile</h1>
+      <div className="max-w-md mx-auto">
+        {saved && (
+          <h2 className="text-center bg-green-100 px-4 py-2 rounded-md my-2 border border-green-300">
+            Profile saved!
+          </h2>
+        )}
+        {isSaving && (
+          <h2 className="text-center bg-blue-100 px-4 py-2 rounded-md my-2 border border-blue-300">
+            Saving...
+          </h2>
+        )}
+        <div className="flex gap-4 items-center">
+          <div>
+            <div className="p-2 rounded-full relative max-w-[120px]">
+              {image && (
+                <Image
+                  className="rounded-full w-full h-full"
+                  src={image}
+                  alt={"avatar"}
+                  width={250}
+                  height={250}
+                />
+              )}
+            </div>
+            <label>
+              <input
+                type="file"
+                className="hidden"
+                onChange={handleFileChange}
+              />
+              <span className="border border-gray-300 block rounded-full px-2 py-1 cursor-poitner text-center">
+                Edit Image
+              </span>
+            </label>
+          </div>
+          <form className="grow" onSubmit={handleProfileInfoUpdate}>
+            <input
+              type="text"
+              placeholder="First and last name"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <input type="email" value={session.data.user.email} disabled />
+            <button type="submit">Save</button>
+          </form>
+        </div>
+      </div>
     </section>
   );
 }
