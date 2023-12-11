5b563c22f6cc3f92f40b60283102c0f7c2461da9
Marketing Director <nicolestark1103@gmail.com>
2023-12-11 15:19:37 -0600

    creating new menu item redirects to all menu items page

---
+++ src/app/menu-items/new/page.js
@@ -0,0 +1,100 @@
+"use client";
+import { useProfile } from "../../../components/UseProfile";
+import { useState } from "react";
+import UserTabs from "../../../components/layout/UserTabs";
+import EditableImage from "../../../components/layout/EditableImage";
+import Link from "next/link";
+import Left from "@/components/icons/Left";
+import { redirect } from "next/navigation";
+import toast from "react-hot-toast";
+
+export default function NewMenuItemPage() {
+  const { loading, data } = useProfile();
+
+  const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [basePrice, setBasePrice] = useState("");
+  const [redirectToItems, setRedirectToItems] = useState(false);
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    const data = { image, name, description, basePrice };
+    const savingPromise = new Promise(async (resolve, reject) => {
+      const response = await fetch("/api/menu-items", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+    await toast.promise(savingPromise, {
+      loading: "Saving this tasty item",
+      success: "Saved",
+      error: "Error saving",
+    });
+
+    setRedirectToItems(true);
+  }
+
+  if (redirectToItems) {
+    return redirect("/menu-items");
+  }
+
+  if (loading) {
+    return "Loading user information...";
+  }
+
+  if (!data.admin) {
+    return "Not authorized...";
+  }
+
+  return (
+    <section className="mt-8">
+      <UserTabs isAdmin={true} />
+      <div className="max-w-md mx-auto mt-8">
+        <Link className="button" href={"/menu-items"}>
+          <Left />
+          <span>Show all menu items</span>
+        </Link>
+      </div>
+      <form onSubmit={handleFormSubmit} className="mt-8 max-w-md mx-auto">
+        <div
+          className="grid gap-4 items-start"
+          style={{ gridTemplateColumns: ".3fr .7fr" }}
+        >
+          <div>
+            <EditableImage link={image} setLink={setImage} />
+          </div>
+          <div className="grow">
+            <label>Item Name</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label>Description</label>
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <label>Base Price</label>
+            <input
+              type="text"
+              value={basePrice}
+              onChange={(e) => setBasePrice(e.target.value)}
+            />
+            <button type="submit">Save</button>
+          </div>
+        </div>
+      </form>
+    </section>
+  );
+}
--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -1,41 +1,11 @@
 "use client";
-import EditableImage from "@/components/layout/EditableImage";
-import UserTabs from "../../components/layout/UserTabs";
 import { useProfile } from "@/components/UseProfile";
-import { useState } from "react";
-import toast from "react-hot-toast";
+import UserTabs from "@/components/layout/UserTabs";
+import Link from "next/link";
+import Right from "@/components/icons/Right";
 
 export default function MenuItemsPage() {
-  const [image, setImage] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [basePrice, setBasePrice] = useState("");
   const { loading, data } = useProfile();
-
-  async function handleFormSubmit(e) {
-    e.preventDefault();
-    const data = { image, name, description, basePrice };
-    const savingPromise = new Promise(async (resolve, reject) => {
-      const response = await fetch("/api/menu-items", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (response.ok) {
-        resolve();
-      } else {
-        reject();
-      }
-    });
-    await toast.promise(savingPromise, {
-      loading: "Saving this tasty item",
-      success: "Saved",
-      error: "Error saving",
-    });
-  }
-
   if (loading) {
     return "Loading user information...";
   }
@@ -45,39 +15,14 @@ export default function MenuItemsPage() {
   }
 
   return (
-    <section className="mt-8">
+    <section className="mt-8 max-w-md mx-auto">
       <UserTabs isAdmin={true} />
-      <form onSubmit={handleFormSubmit} className="mt-8 max-w-md mx-auto">
-        <div
-          className="grid gap-4 items-start"
-          style={{ gridTemplateColumns: ".3fr .7fr" }}
-        >
-          <div>
-            <EditableImage link={image} setLink={setImage} />
-          </div>
-          <div className="grow">
-            <label>Item Name</label>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <label>Description</label>
-            <input
-              type="text"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            <label>Base Price</label>
-            <input
-              type="text"
-              value={basePrice}
-              onChange={(e) => setBasePrice(e.target.value)}
-            />
-            <button type="submit">Save</button>
-          </div>
-        </div>
-      </form>
+      <div className="mt-8 flex">
+        <Link className="button" href={"/menu-items/new"}>
+          Create New Menu Item
+          <Right />
+        </Link>
+      </div>
     </section>
   );
 }
