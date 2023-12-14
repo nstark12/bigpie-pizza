fa35ae9f51dc3d463370c6ab4d4757e203b71302
Marketing Director <nicolestark1103@gmail.com>
2023-12-14 09:42:05 -0600

    test

--- src/libs/isAdmin.js
+++ src/libs/isAdmin.js
@@ -1,5 +1,6 @@
 import { UserInfo } from "@/models/UserInfo";
 import { authOptions } from "./authOptions";
+import { getServerSession } from "next-auth";
 
 export async function isAdmin() {
   const session = await getServerSession(authOptions);
